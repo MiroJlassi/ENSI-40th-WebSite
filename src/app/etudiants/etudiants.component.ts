45c0a7d2cbd9ae569960e6fdf18730abd1f6c56c
Amel TRIKI <amel.triki@gmail.com>
2022-10-01 15:24:45 +0200

    TP3 Complément Finalisé

--- src/app/etudiants/etudiants.component.ts
+++ src/app/etudiants/etudiants.component.ts
@@ -7,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EtudiantsComponent implements OnInit {
   etudiants = [
-    { nom: "Sakli", prenom:"Eya", photo:"assets/Eya.jpg", dateNaissance:new Date(2002, 9,5), moyenne:12.37, boursier: false, ville:"Tunis"},
-    { nom: "Mrad", prenom:"Yassine",photo:"assets/Yassine.jpg",  dateNaissance:new Date(2001, 4,28), moyenne:9.95 , boursier: false, ville:"Nabeul"},
-    { nom: "Safi", prenom:"Sarra",photo:"assets/Sarra.jpg",  dateNaissance:new Date(2003,2,3), moyenne:14.58, boursier: true, ville:"Sfax"},
-    { nom: "Mensi", prenom:"Omar",photo:"assets/Omar.jpg",  dateNaissance:new Date(2002, 8,15), moyenne:16.77, boursier: true, ville:"Kef"}
+    { nom: "Sakli", prenom:"Eya", photo:"assets/Eya.jpg", dateNaissance:new Date(2002, 9,5), moyenne:12.37, boursier: false, ville:"Tunis", option:"DSI"},
+    { nom: "Mrad", prenom:"Yassine",photo:"assets/Yassine.jpg",  dateNaissance:new Date(2001, 4,28), moyenne:9.95 , boursier: false, ville:"Nabeul", option:"RSI"},
+    { nom: "Safi", prenom:"Sarra",photo:"assets/Sarra.jpg",  dateNaissance:new Date(2003,2,3), moyenne:14.58, boursier: true, ville:"Sfax", option:"DSI"},
+    { nom: "Mensi", prenom:"Omar",photo:"assets/Omar.jpg",  dateNaissance:new Date(2002, 8,15), moyenne:16.77, boursier: true, ville:"Kef", option:"SEM"}
   ];
   afficher:boolean = false;
   infos:string="";
